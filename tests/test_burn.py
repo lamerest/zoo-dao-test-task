6f9ce2e41b4024b39f18fb7a833fa9322ad75429
PROkrastinator <vladykin.190103@gmail.com>
2022-05-21 12:27:15 +0300

    Wrote test for burn function

---
+++ tests/test_burn.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import brownie
+
+null_address = '0x0000000000000000000000000000000000000000'
+
+def test_sender_balance_decreases(accounts, token):
+    sender_balance = token.balanceOf(accounts[0])
+    amount = sender_balance // 4
+
+    token.burn(accounts[0], amount, {'from': accounts[0]})
+
+    assert token.balanceOf(accounts[0]) == sender_balance - amount
+
+
+def test_burn_full_balance(accounts, token):
+    sender_balance = token.balanceOf(accounts[0])
+    amount = sender_balance
+
+    token.burn(accounts[0], amount, {'from': accounts[0]})
+
+    assert token.balanceOf(accounts[0]) == 0
+    
+
+def test_total_supply_affected(accounts, token):
+    total_supply = token.totalSupply()
+    amount = token.balanceOf(accounts[0])
+
+    token.burn(accounts[0], amount, {'from': accounts[0]})
+
+    assert token.totalSupply() == total_supply - amount
+
+
+
+def test_burn_zero_tokens(accounts, token):
+    sender_balance = token.balanceOf(accounts[0])
+
+    token.burn(accounts[0], 0, {'from': accounts[0]})
+
+    assert token.balanceOf(accounts[0]) == sender_balance
+
+
+def test_insufficient_balance(accounts, token):
+    balance = token.balanceOf(accounts[0])
+
+    with brownie.reverts():
+        token.burn(accounts[0], balance + 1, {'from': accounts[0]})
+
+def test_transfer_event_fires(accounts, token):
+    amount = token.balanceOf(accounts[0])
+
+    tx = token.burn(accounts[0], amount, {'from': accounts[0]})
+
+    assert len(tx.events) == 1
+    assert tx.events["Transfer"].values() == [accounts[0], null_address, amount]
