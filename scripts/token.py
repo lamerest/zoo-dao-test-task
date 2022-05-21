a97ef1f869b432d964affd693706cb5a220be19d
PROkrastinator <vladykin.190103@gmail.com>
2022-05-21 12:46:09 +0300

    Appended whitelisted accounts up to 4

--- scripts/token.py
+++ scripts/token.py
@@ -8,7 +8,7 @@ symbol = "ZDT"
 decimals = 18
 
 def main():
-    whitelist = ['0x9Ac2Fa5e617663B9cF4E77af38bb89d02BD307c4', '0xd7067E6AcC2B703df74b7c83464c4dfB2Ee27d43', '0x84c59FeC3a0022d37eB56dfA6b8e98AC24853732']
+    whitelist = ['0x9Ac2Fa5e617663B9cF4E77af38bb89d02BD307c4', '0xd7067E6AcC2B703df74b7c83464c4dfB2Ee27d43', '0x84c59FeC3a0022d37eB56dfA6b8e98AC24853732', '0xf3A4668E43258c98B60313a5de7604Fb0A7416A5']
     account = accounts.add(config["wallets"]["from_key"])
 
     overrides = {
